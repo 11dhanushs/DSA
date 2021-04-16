b7a933c082a84ff70edbd332d46e8039f1fe4af0
Dhanush <e19cse011@bennett.edu.in>
2021-04-16 17:19:27 +0530

    C++ Implementation of Finding the minimum element in a sorted and rotated array using divide and conquer principle

---
+++ minimumElementInSortedAndRotatedArray.cpp
@@ -0,0 +1,89 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h> 
+#include <complex>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <list>
+#include <chrono>
+#include <random>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <iomanip>
+#include <fstream>
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> p32;
+typedef pair<ll,ll> p64;
+typedef pair<double,double> pdd;
+typedef vector<ll> v64;
+typedef vector<int> v32;
+typedef vector<vector<int> > vv32;
+typedef vector<vector<ll> > vv64;
+typedef vector<vector<p64> > vvp64;
+typedef vector<p64> vp64;
+typedef vector<p32> vp32;
+ll MOD = 998244353;
+double eps = 1e-12;
+#define forn(i,e) for(ll i = 0; i < e; i++)
+#define forsn(i,s,e) for(ll i = s; i < e; i++)
+#define rforn(i,s) for(ll i = s; i >= 0; i--)
+#define rforsn(i,s,e) for(ll i = s; i >= e; i--)
+#define ln "\n"
+#define dbg(x) cout<<#x<<" = "<<x<<ln
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF 2e18
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+ 
+
+int solve(int arr[],int n){
+    int l=0,r = n-1,mid = (l+r)/2;
+    while(l<=r)
+    {
+        if(arr[l]<arr[r])
+        {
+            return arr[l];
+        }
+        if(mid==0||mid==n-1||arr[mid]<=arr[mid-1])
+        {
+            return arr[mid];
+        }
+        else if(arr[mid]>arr[mid-11])
+        {
+            l = mid+1;
+        }
+        else
+        {
+            r = mid-1;
+        }
+    }
+    return -1;
+}
+int main()
+{
+ int n;
+ cin>>n;
+ int arr[n];
+ for(int i=0;i<n;i++)
+ {
+     cin>>arr[i];
+ }
+ cout<<solve(arr,n);
+ return 0;
+}
